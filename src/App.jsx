7994fbcc28c4c8144d2c673baa4dea81b5eb4552
itskvsin <kevinsolankistudy@gmail.com>
2025-12-03 01:16:35 +0530

    removed unnecessary file
    restarted the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-import Navbar from "./Components/Navbar";
-import Hero from "./Components/Hero";
-import Project from "./Components/Project";
-import projects  from "./Utils/Projects";
-import skillCategories from "./Utils/SkillCategories";
-import Skills from "./Components/Skills";
-import Contact from "./Components/Contact";
-import MeetYourDeveloper from "./Components/MeetYourDeveloper";
-
-
 export default function App() {
   return (
     <div>
-      <Navbar />
-      <Hero />
-      <Project projects={projects}/>
-      <Skills skillCategories={skillCategories}/>
-      <MeetYourDeveloper />
-      <Contact />
+      hi
     </div>
   );
 }
